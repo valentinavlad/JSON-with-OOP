12c2c63ec0af53f91f21eba56efef21b1c94a04d
Valentina Vlad <vlad.valentina.diana@gmail.com>
2019-12-17 10:56:18 +0200

    added Optional class

---
+++ RangeTests/Optional.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RangeTests
+{
+    class Optional
+    {
+        IPattern pattern;
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string textToBeConsumed)
+        {
+            string textUnConsumed = textToBeConsumed;
+            var match = pattern.Match(textToBeConsumed);
+            textToBeConsumed = match.RemainingText();
+
+            return !match.Success() 
+                   ? new SuccessMatch(textUnConsumed) 
+                   : match;
+        }
+    }
+}
---
+++ RangeTests/OptionalTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RangeTests
+{
+    public class OptionalTests
+    {
+        /*var a = new Optional(new Character('a'));
+        a.Match("abc"); // true / "bc"
+        a.Match("aabc"); // true / "abc"
+        a.Match("bc"); // true / "bc"
+        a.Match(""); // true / ""
+        a.Match(null); // true / null*/
+
+        [Theory]
+        [InlineData("abc")]
+        [InlineData("aabc")]
+        [InlineData("bc")]
+        [InlineData("")]
+        [InlineData(null)]
+
+        public void CheckIfPatternsReturnTrue(string input)
+        {
+            var a = new Optional(new Character('a'));
+            Assert.True(a.Match(input).Success());
+        }
+
+        [Fact]
+
+        public void CheckIfTextIsConsumed2ShouldReturnTrue()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.Equal("bc", a.Match("abc").RemainingText());
+        }
+
+
+    }
+}
