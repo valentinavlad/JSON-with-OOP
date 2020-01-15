7f08501f08dd31695f1b0222d4186f07df52f48c
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-01-15 11:14:29 +0200

    addesd Value Class

---
+++ RangeTests/Value.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RangeTests
+{
+    class Value : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Value()
+        {
+            var ws = new Many(new Any(" \r\n\t"));
+            var strring = new String();
+            var number = new Number();
+            var value = new Choice(
+                    strring,
+                    number,
+                    new Text("true"),
+                    new Text("false"),
+                    new Text("null")
+            );
+            var element = new Sequence(ws,value,ws);
+            var elements = new List(element, new Character(','));
+          
+            var member = new Sequence(
+                ws,
+                strring,
+                ws,
+                new Character(':'),
+                element
+            );
+            var members = new List(member, new Character(','));
+            var array = new Choice(
+                 new Sequence(
+                     new Character('['),
+                     ws,
+                     new Character(']')),
+                 new Sequence(
+                     new Character('['),
+                     elements,
+                     new Character(']'))
+             );
+
+            var objecct = new Choice(
+                new Sequence(
+                    new Character('{'),
+                    ws,
+                    new Character('}')),
+                new Sequence(
+                    new Character('{'),
+                    members,
+                    new Character('}'))
+            );
+
+            value.Add(array);
+            value.Add(objecct);
+
+            pattern = value;
+        }
+        
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+        
+    }
+}
---
+++ RangeTests/ValueTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RangeTests
+{
+    public class ValueTests
+    {
+        [Theory]
+        [InlineData("true")]
+        [InlineData("false")]
+        [InlineData("null")]
+        [InlineData("12")]
+        [InlineData("\"abgr\"")]
+        [InlineData("{ }")]
+        [InlineData("{ \"key\" : \"pair\" }")]
+        [InlineData("{ \"1\" : 125, \"2\" : 56 }")]
+        [InlineData("[ ]")]
+        [InlineData("[ 5,6,7]")]
+        [InlineData("[ true, false ]")]
+        [InlineData("[ { \"1\" : 125, \"2\" : 56 },{ \"3\" : 125, \"4\" : 5 } ]")]
+
+        public void CheckIfValueReturnTrue(string input)
+        {
+            var value = new Value();
+            Assert.True(value.Match(input).Success());
+        }
+/*
+        [Theory]
+        [InlineData("true")]
+        [InlineData("false")]
+        [InlineData("null")]
+
+        public void CheckIfValueReturnFalse(string input)
+        {
+            var value = new Value();
+            Assert.False(value.Match(input).Success());
+        }
+
+    */
+
+    }
+}
